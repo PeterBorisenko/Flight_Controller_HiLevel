32d68b0b49babc4c05b5b87ebebe1e40b8295f9c
Peter Hirt <peter@awesome39.com>
2015-02-05 23:56:00 +0200

    Extended

--- Assign.h
+++ Assign.h
@@ -49,10 +49,52 @@ typedef int16_t vect_t;
 
 // GPS NMEA Pins/Ports
     // Needed Software USART !
-#define GPS_port PORTB
-#define GPS_dir DDRB
+#define GPS_port		PORTB
+#define GPS_dir			DDRB
 #define GpsRX_McuTX_pin PINB4
 #define GpsTX_McuRX_pin PINB3
 #define GpsStatus_pin	PINB2
+
+// Sonar Pins/Ports
+#define SONAR_DIR		DDRB
+#define SONAR_PORT		PORTB
+#define SONAR_PIN		PINB0 // Arduino mini pin 8
+
+// Gear switch Pins/Ports
+#define GEAR_DIR
+#define GEAR_PORT
+#define GEAR_PIN		// Arduino mini pin 9
+
+// Proximity Pins/Ports
+#define F_PROX_DIR
+#define F_PROX_PORT
+#define F_PROX_PIN			// ADC7
+#define F_PROX_ADC		
+#define F_PROX_ADC_MUX
+#define B_PROX_DIR
+#define B_PROX_PORT
+#define B_PROX_PIN			// ADC6
+#define B_PROX_ADC
+#define B_PROX_ADC_MUX
+#define L_PROX_DIR
+#define L_PROX_PORT
+#define L_PROX_PIN			// ADC0
+#define L_PROX_ADC		ADC0D
+#define L_PROX_ADC_MUX
+#define R_PROX_DIR
+#define R_PROX_PORT
+#define R_PROX_PIN			// ADC1
+#define R_PROX_ADC		ADC1D
+#define R_PROX_ADC_MUX
+#define U_PROX_DIR
+#define U_PROX_PORT
+#define U_PROX_PIN			// ADC2
+#define U_PROX_ADC		ADC2D
+#define U_PROX_ADC_MUX
+#define D_PROX_DIR
+#define D_PROX_PORT
+#define D_PROX_PIN			// ADC3
+#define D_PROX_ADC		ADC3D
+#define D_PROX_ADC_MUX
 
 #endif /* ASSIGN_H_ */
