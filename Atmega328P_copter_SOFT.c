e534604ad4eece80e7a8ecdbc4fde2765ddf0b68
Peter Hirt <peter@awesome39.com>
2015-02-13 21:32:48 +0200

    Special sending algorhitm. Used 2 USART states IDLE and WORKING

--- Atmega328P_copter_SOFT.c
+++ Atmega328P_copter_SOFT.c
@@ -19,25 +19,36 @@
 #include "System.h"
 #include "Communication.h"
 
-volatile uint8_t transmitByteCount= DATA_WIDTH;
+
+volatile const uint8_t sendDataLength= 13;
+volatile uint8_t sendBufferIndex= 0;
+volatile usartState_t usartState;
 
 volatile static uint8_t FLAGS= 0x00;
 
 // Vector instructions for drive system
-typedef union {
+typedef struct  
+{
 	vect_float_t required_vect;
-	uint8_t byteToSend[4];
-} Required_t;
+	int8_t rotation;
+} Motion_t;
 
-Required_t Required;
+typedef union {
+	Motion_t motion;
+	uint8_t byteToSend[13];
+} Instruction_t;
+
+Instruction_t Instruction;
 Baro_t Baro;
 LSM303_t Compass;
 
-volatile static Required_t * pRequired= &Required;
+volatile static Instruction_t * pInstruction= &Instruction;
 Baro_t * pBaro= &Baro;
 LSM303_t * pCompass= &Compass;
 
 CPPM_In_t cppm[6];
+
+uint8_t buffer[13];
 
 void getAltitude() {
 	BMP085Convert(pBaro);
@@ -51,18 +62,27 @@ void main(void)
 	prepareSonar();
 	prepareCompass(pCompass);
 	
-    while(1)
-    {
+    while(1) {
         //TODO:: Please write your application code 
     }
 }
 
-void sendMessage(Required_t req) {
-	
+void startSending() {
+	sendChar(HEADER&0xFF);
+	uartIntOn(USART_UD_INT);
+}
+
+void sendMessage(Instruction_t * msg, uint8_t * buf) {
+	for (uint8_t i= 0; i<= 13; i++) {
+		buf[i]= msg->byteToSend[i];
+	}
+	sendBufferIndex++;
+	sendChar(buf[0]);
+	uartIntOn(USART_UD_INT);
 }
 
 ISR(TIMER0_OVF_vect){ 
-
+	
 }
 
 ISR(TIMER2_OVF_vect){ // System TIMER
@@ -72,15 +92,67 @@ ISR(TIMER2_OVF_vect){ // System TIMER
 
 ISR (USART_TX_vect) {
 	// if TX completed - wait for answer
+	if (usartState == USART_WORK) {
+		switch(sendBufferIndex) {
+			case 4: case 8: case 12: case 13:
+				// TODO: start ACK receive timer
+				break;
+			default:
+				break;
+		}
+	}
 }
 
 ISR (USART_UDRE_vect) {
 	// if DR empty - place next byte if exists or disable UDRE int
+	if (usartState == USART_WORK) {
+		switch(sendBufferIndex){
+			case 13:
+				sendBufferIndex= 0;
+				uartIntOff(USART_UD_INT);
+				break;
+			case 3: case 7: case 11: case 12: // previously sent byte
+				sendChar(buffer[sendBufferIndex]);
+				uartIntOff(USART_UD_INT);
+				break;
+			default:
+				sendChar(buffer[sendBufferIndex]);
+				sendBufferIndex++;
+				break;
+		}
+	}
+	else if (usartState == USART_IDLE) {
+		sendChar((HEADER >> 8)&0xFF);
+		uartIntOff(USART_UD_INT);
+	}
 }
 
 ISR (USART_RX_vect) {
 	// if received ACK - send next message if exists or change state
 	// if received NACK - send message from beginning
+	uint8_t resp= receiveChar();
+	if (usartState == USART_WORK) {
+		if (resp == ACK) {
+			sendBufferIndex++;
+			sendChar(buffer[sendBufferIndex]);
+			uartIntOn(USART_UD_INT);
+		} 
+		else if (resp == NACK) {
+			// TODO: error processing
+			sendBufferIndex= 0;
+			sendChar(buffer[sendBufferIndex]);
+			uartIntOn(USART_UD_INT);
+		}
+	}
+	else if (usartState == USART_IDLE) {
+		if (resp == ACK) {
+			sendMessage(pInstruction, buffer);
+		} 
+		else if (resp == NACK) {
+			// TODO: error processing
+			startSending();
+		}
+	}
 }
 
 ISR(WDT_vect) {
