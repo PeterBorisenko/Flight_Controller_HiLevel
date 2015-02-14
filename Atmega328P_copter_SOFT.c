b04fffe3a44487dc01081846a27feda861f8fc58
Peter Hirt <peter@awesome39.com>
2015-02-15 01:59:37 +0200

    Commented

--- Atmega328P_copter_SOFT.c
+++ Atmega328P_copter_SOFT.c
@@ -86,11 +86,14 @@ void TASK_(void * task) { // task container
 	
 }
 
+ // Begin of a message sending
+ // Sends first byte of header and turns ON UD interrupt
 void startSending() {
 	sendChar(HEADER&0xFF);
 	uartIntOn(USART_UD_INT);
 }
 
+ // Sends 
 void sendMessage(Instruction_t * msg, uint8_t * buf) {
 	for (uint8_t i= 0; i<= 13; i++) {
 		buf[i]= msg->byteToSend[i];
@@ -123,24 +126,24 @@ ISR (USART_TX_vect) {
 }
 
 ISR (USART_UDRE_vect) {
-	// if DR empty - place next byte if exists or disable UDRE int
-	if (usartState == USART_WORK) {
-		switch(sendBufferIndex){
-			case 13:
+	if (usartState == USART_WORK) {				
+		switch(sendBufferIndex){				// previously sent byte
+			case 13:								// if was sent the last byte - turn OFF UD interrupt
 				sendBufferIndex= 0;
 				uartIntOff(USART_UD_INT);
 				break;
-			case 3: case 7: case 11: case 12: // previously sent byte
+			case 3: case 7: case 11: case 12:		// if next byte is the last byte of vector type element - send next and turn OFF UD interrupt
 				sendChar(buffer[sendBufferIndex]);
 				uartIntOff(USART_UD_INT);
 				break;
-			default:
+			default:								// just send the next byte from the buffer
 				sendChar(buffer[sendBufferIndex]);
-				sendBufferIndex++;
+				sendBufferIndex++;					// increment counter after sending !!!
 				break;
 		}
+		return;
 	}
-	else if (usartState == USART_IDLE) {
+	else if (usartState == USART_IDLE) { // Sends the second byte of message header
 		sendChar((HEADER >> 8)&0xFF);
 		uartIntOff(USART_UD_INT);
 	}
@@ -151,20 +154,29 @@ ISR (USART_RX_vect) {
 	// if received NACK - send message from beginning
 	uint8_t resp= receiveChar();
 	if (usartState == USART_WORK) {
-		if (resp == ACK) {
-			sendBufferIndex++;
-			sendChar(buffer[sendBufferIndex]);
-			uartIntOn(USART_UD_INT);
+		if (resp == ACK) {				
+			if (sendBufferIndex != 13) {			// if received ACK and that was not after sending the last byte - send next
+				sendBufferIndex++;
+				sendChar(buffer[sendBufferIndex]);
+				uartIntOn(USART_UD_INT);
+			} 
+			else {									// if received ACK and that was after sending the last byte - change state and exit				
+				sendBufferIndex= 0;
+				usartState= USART_IDLE;
+				return;
+			}
 		} 
 		else if (resp == NACK) {
 			// TODO: error processing
 			sendBufferIndex= 0;
 			sendChar(buffer[sendBufferIndex]);
 			uartIntOn(USART_UD_INT);
+			return;
 		}
 	}
 	else if (usartState == USART_IDLE) {
 		if (resp == ACK) {
+			usartState= USART_WORK;
 			sendMessage(pInstruction, buffer);
 		} 
 		else if (resp == NACK) {
